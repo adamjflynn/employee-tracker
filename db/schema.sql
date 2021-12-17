a9196077c97c4d8ec659a4a27f7646f7a7e5517a
Adam Flynn <adamflynn1@gmail.com>
2021-12-16 20:22:56 -0600

    install necessary packages and setup and seed the database

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
+CREATE TABLE manager (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (name)
+VALUES
+    (1, 'Management'),
+    (2, 'Finance'),
+    (3, 'IT'),
+    (4, 'Sales'),
+    (5, 'Human Resources');
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES 
+    (1, 'Manager', 120000, 1),
+    (2, 'Accountant', 100000, 2),
+    (3, 'Engineer', 130000, 3),
+    (4, 'Account Executive', 90000, 4),
+    (5, 'HR Manager', 75000, 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('Adam', 'Flynn', 3, 1),
+    ('Clark', 'Griswold', 4, 2),
+    ('Kris', 'Kringle', 5, 1),
+    ('Kevin', 'McAllister', 4, 2),
+    ('Bob', 'Crachitt', 2, 2),
+    ('Yukon', 'Cornelius', 4, 1),
+    ('George', 'Bailey', 1, NULL),
+    ('Ebeneezer', 'Scrooge', 1, NULL);
+
+INSERT INTO manager
+VALUES
+    ('George', 'Bailey', 1),
+    ('Ebeneezer', 'Scrooge', 2);
